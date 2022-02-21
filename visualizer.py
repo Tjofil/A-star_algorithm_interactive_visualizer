3b851a1e39445ec051add15ea5e711db299df2f8
Tjofil <df190315d@student.etf.bg.ac.rs>
2022-02-21 04:10:01 +0100

    grid and painting

---
+++ visualizer.py
@@ -0,0 +1,74 @@
+import pygame
+pygame.init()
+
+WIDTH, HEIGHT = 800, 800
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("A* Visualizer")
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (0, 128, 0)
+
+NUM_COL, NUM_ROW = 50, 50
+VER_WIDTH, VER_HEIGHT = WIDTH//NUM_COL, HEIGHT//NUM_ROW
+
+
+
+class Vertex:
+    DEF_COLOR = WHITE
+    OBS_COLOR = GREEN
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.color = self.DEF_COLOR
+    def display(self, win):
+        pygame.draw.rect(win, self.color ,(self.x*VER_WIDTH, self.y*VER_HEIGHT, VER_WIDTH-2, VER_HEIGHT-2) )
+        #print(f'{self.x*VER_WIDTH}, {self.y*VER_HEIGHT} tried to be drawn.')
+
+    def set_obstacle(self):
+        self.color = self.OBS_COLOR
+
+
+
+
+
+def draw(win, grid):
+    win.fill(BLACK)
+    for i in range(NUM_ROW):
+        for j in range(NUM_COL):
+            grid[i][j].display(win)
+    pygame.display.update()
+
+def main():
+
+    grid = [0 for i in range(NUM_ROW)]
+    for i in range(NUM_ROW):
+        grid[i] = [0 for i in range(NUM_COL)]
+
+    for i in range(NUM_ROW):
+        for j in range(NUM_COL):
+            grid[i][j] = Vertex(i , j)
+
+    run = True
+
+    draw(WIN, grid)
+   
+    while run:
+               
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+            elif event.type == pygame.MOUSEBUTTONUP:
+                pos = pygame.mouse.get_pos()
+                grid[pos[0]//VER_WIDTH][pos[1]//VER_HEIGHT].set_obstacle()
+                print(f'Tried obsing {pos[0]//VER_WIDTH}, {pos[1]//VER_HEIGHT}')
+                draw(WIN, grid)
+
+
+
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
